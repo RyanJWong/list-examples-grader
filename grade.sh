05e1276a641f13c68af9365111f21cda76f2aa13
ryanjwong <ryanjwong007@gmail.com>
2023-11-28 16:40:44 -0800

    init

--- grade.sh
+++ grade.sh
@@ -1,4 +1,5 @@
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+FILE='./student-submission/ListExamples.java'
 
 rm -rf student-submission
 rm -rf grading-area
@@ -8,7 +9,22 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+if [[ -f $FILE ]]
+then
+    cp GradeServer.java Server.java TestListExamples.java student-submission/ListExamples.java grading-area 
+    cd grading-area
+    javac -cp $CPATH *.java 2> errors.txt
+    if [[ $? -eq 0 ]]
+    then
+        java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > result.txt
 
+    else
+        echo 'ListExamples.java failed to compile, please check your code'
+        exit
+    fi
+    else 
+        echo 'ListExamples.java not found, please resubmit with the correct file'
+fi
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
